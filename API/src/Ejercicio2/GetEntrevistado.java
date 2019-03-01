cb578ec002162f8408156107340cff6e63f03b7c
Diego <Diego@192.168.0.68>
2019-03-01 00:03:00 -0300

    Primer Commit

---
+++ API/src/Ejercicio2/GetEntrevistado.java
@@ -0,0 +1,20 @@
+package Ejercicio2;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+ 
+@Path("/candidate")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class GetEntrevistado {
+           
+  @GET
+  public Response candidate() {
+    return Response.ok("Buen dia mi nombre es Diego",MediaType.APPLICATION_JSON).build();
+ 
+  }
+}
---
+++ API/src/Ejercicio2/RestAplication.java
@@ -0,0 +1,10 @@
+package Ejercicio2;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/")
+public class RestAplication extends Application {
+
+}
